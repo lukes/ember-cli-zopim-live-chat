edbdb642cb656463f585c29e7feb5ba472540cf4
Luke Duncalfe <lduncalfe@eml.cc>
2014-12-15 12:04:41 +1300

    Returning arrays of strings

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 'use strict';
 
-zopimCore = function(config) {
+function zopimCore(config) {
   var id = config.id;
   return [
-    "<script type='text/javascript'>
-      window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
-      d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-      _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-      $.src='//v2.zopim.com/?"+id+"';z.t=+new Date;$.
-      type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    </script>"
+    "<script type='text/javascript'>",
+    "window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=",
+    "d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.",
+    "_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');",
+    "$.src='//v2.zopim.com/?"+id+"';z.t=+new Date;$.",
+    "type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');",
+    "</script>"
   ];
 }
 
-zopimWindow = function(config) {
-  if (config === null) {
+function zopimWindow(config) {
+  if (!config) {
     return [];
   }
 
@@ -35,14 +35,14 @@ zopimWindow = function(config) {
   }
 
   return [
-    "<script type='text/javascript'>
-      $(document).ready(function() {
-        $zopim(function() {
-          "+settings.join("\n")+"
-        });
-      });
-    </script>"
-  ]
+    "<script type='text/javascript'>",
+    "$(document).ready(function() {",
+    "$zopim(function() {",
+    settings.join("\n"),
+    "});",
+    "});",
+    "</script>"
+  ];
 }
 
 module.exports = {
